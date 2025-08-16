5a89456c0fc2cb7ff74c856d8163b9ceb0bab422
Leonardo Garcia Tampelini <tampelini@gmail.com>
2025-08-16 01:40:08 -0300

    Atualização do projeto

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
-# wsgi.py
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))  # Render fornece a porta
+    app.run(host="0.0.0.0", port=port)
